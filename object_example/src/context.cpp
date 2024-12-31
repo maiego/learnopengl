4b4794c67a3ef46fc843a21cf5ee5bea59b6c6e1
ryongdev <maiego@naver.com>
2024-12-31 18:16:39 +0900

    object_example 추가
    Mesh class 추가, Material class 추가, model class 추가
    assimp 로 model 불러와서 렌더링

---
+++ object_example/src/context.cpp
@@ -0,0 +1,226 @@
+#include "context.h"
+#include "imgui.h"
+
+ContextUPtr Context::Create()
+{
+    auto context = ContextUPtr(new Context());
+    if (!context->Init())
+        return nullptr;
+    return std::move(context);
+}
+
+bool Context::Init()
+{
+    m_box = Mesh::CreateBox();
+    m_model = Model::Load("/backpack/backpack.obj");
+    if (!m_model)
+    {
+        return false;
+    }
+
+    m_simpleProgram = Program::Create("/simple.vs", "/simple.fs");
+    if (m_simpleProgram == nullptr)
+    {
+        return false;
+    }
+    SPDLOG_INFO("simpleProgram id: {}", m_simpleProgram->Get());
+
+    m_program = Program::Create("/lighting.vs", "/spot_lighting.fs");
+    if (m_program == nullptr)
+    {
+        return false;
+    }
+    SPDLOG_INFO("program id: {}", m_program->Get());
+
+    glClearColor(0.1f, 0.2f, 0.3f, 0.0f);
+    //glEnable(GL_MULTISAMPLE);
+
+    m_material.diffuse = Texture::CreateFromImage(
+        Image::CreateSingleColorImage(4,4, glm::vec4(1.0f,1.0f,1.0f,1.0f)).get());
+    m_material.specular = Texture::CreateFromImage(
+        Image::CreateSingleColorImage(4,4, glm::vec4(0.5f, 0.5f, 0.5f, 1.0f)).get());
+
+    return true;
+}
+
+void Context::Render()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glEnable(GL_DEPTH_TEST);
+
+    if (ImGui::Begin("UI window"))
+    {
+        if (ImGui::ColorEdit4("clear color", glm::value_ptr(m_clearColor)))
+        {
+            glClearColor(m_clearColor.r, m_clearColor.g, m_clearColor.b, m_clearColor.a);
+        }
+        ImGui::Separator();
+
+        ImGui::DragFloat3("camera pos", glm::value_ptr(m_cameraPos), 0.01f);
+        ImGui::DragFloat("camera yaw", &m_cameraYaw, 0.5f);
+        ImGui::DragFloat("camera pitch", &m_cameraPitch, 0.5f, -89, 89);
+        ImGui::Separator();
+
+        if (ImGui::Button("Reset Camera"))
+        {
+            m_cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+            m_cameraYaw = 0.0f;
+            m_cameraPitch = 0.0f;
+        }
+
+        if (ImGui::CollapsingHeader("light", ImGuiTreeNodeFlags_DefaultOpen))
+        {
+            ImGui::DragFloat3("l.position", glm::value_ptr(m_light.position), 0.01f);
+            ImGui::DragFloat3("l.direction", glm::value_ptr(m_light.direction), 0.01f);
+            ImGui::DragFloat2("l.cutoff", glm::value_ptr(m_light.cutoff), 0.01f);
+            ImGui::DragFloat("l.distance", &m_light.distance, 0.05f, 0.0f, 3000.0f);
+            ImGui::ColorEdit3("l.ambient", glm::value_ptr(m_light.ambient), 0.01f);
+            ImGui::ColorEdit3("l.diffuse", glm::value_ptr(m_light.diffuse), 0.01f);
+            ImGui::ColorEdit3("l.specular", glm::value_ptr(m_light.specular), 0.01f);
+        }
+
+        if (ImGui::CollapsingHeader("material", ImGuiTreeNodeFlags_DefaultOpen))
+        {
+            ImGui::DragFloat("m.shininess", &m_material.shininess, 1.0f, 1.0f, 256.0f);
+        }
+        
+        ImGui::Checkbox("animation", &m_animation);
+        ImGui::Separator();
+
+        std::string fps = "FPS : " + std::to_string((int)ImGui::GetIO().Framerate);
+        ImGui::Text(fps.c_str());
+    }
+    ImGui::End();
+    
+    m_cameraDir =
+        glm::rotate(glm::mat4(1.0f), glm::radians(m_cameraYaw), glm::vec3(0.0f, 1.0f, 0.0f)) *
+        glm::rotate(glm::mat4(1.0f), glm::radians(m_cameraPitch), glm::vec3(1.0f, 0.0f, 0.0f)) *
+        glm::vec4(0.0f, 0.0f, -1.0f, 0.0f);
+
+    auto view = glm::lookAt(m_cameraPos, m_cameraPos + m_cameraDir, m_cameraUp);
+    auto projection = glm::perspective(glm::radians(45.0f),
+        static_cast<float>(m_width) / static_cast<float>(m_height), 0.01f, 100.0f);
+
+    // light 에 cube 그리기
+    {
+        auto lightModelTransform = 
+            glm::translate(glm::mat4(1.0f), m_light.position) *
+            glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+        
+        m_simpleProgram->Use();
+        m_simpleProgram->SetUniform("color", glm::vec4(m_light.ambient + m_light.diffuse, 1.0f));
+        m_simpleProgram->SetUniform("transform", projection * view * lightModelTransform);
+        
+        m_box->Draw(m_simpleProgram.get());
+    }
+
+    m_program->Use();
+    m_program->SetUniform("viewPos", m_cameraPos);
+    m_program->SetUniform("light.position", m_light.position);
+    m_program->SetUniform("light.direction", m_light.direction);
+    m_program->SetUniform("light.cutoff", glm::vec2(
+        cosf(glm::radians(m_light.cutoff[0])),
+        cosf(glm::radians(m_light.cutoff[0] + m_light.cutoff[1]))));
+    m_program->SetUniform("light.attenuation", GetAttenuationCoeff(m_light.distance));
+    //m_program->SetUniform("light.direction", m_light.direction);
+    m_program->SetUniform("light.ambient", m_light.ambient);
+    m_program->SetUniform("light.diffuse", m_light.diffuse);
+    m_program->SetUniform("light.specular", m_light.specular);
+    
+    m_program->SetUniform("material.diffuse", 0);
+    m_program->SetUniform("material.specular", 1);
+    m_program->SetUniform("material.shininess", m_material.shininess);
+
+    glActiveTexture(GL_TEXTURE0);
+    m_material.diffuse->Bind();
+
+    glActiveTexture(GL_TEXTURE1);
+    m_material.specular->Bind();
+
+    auto modelTransform = glm::mat4(1.0f);
+    auto transform = projection * view * modelTransform;
+
+    m_program->SetUniform("transform", transform);
+    m_program->SetUniform("modelTransform", modelTransform);
+    m_model->Draw(m_program.get());
+}
+
+void Context::ProcessInput(GLFWwindow* window)
+{
+    const float cameraSpeed = 0.05f;
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        m_cameraPos += cameraSpeed * m_cameraDir;
+    
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        m_cameraPos -= cameraSpeed * m_cameraDir;
+
+
+    auto cameraRight = glm::normalize(glm::cross(m_cameraUp, -m_cameraDir));
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        m_cameraPos += cameraSpeed * cameraRight;
+
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        m_cameraPos -= cameraSpeed * cameraRight;
+
+
+    auto cameraUp = glm::normalize(glm::cross(-m_cameraDir, cameraRight));
+
+    if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
+        m_cameraPos += cameraSpeed * cameraUp;
+    if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
+        m_cameraPos -= cameraSpeed * cameraUp;
+
+
+    if (!m_cameraControl)
+        return;
+}
+
+void Context::Reshape(int width, int height)
+{
+    m_width = width;
+    m_height = height;
+    glViewport(0, 0, width, height);
+}
+
+void Context::MouseMove(double x, double y)
+{
+    if (!m_cameraControl)
+        return;
+
+    auto pos = glm::vec2(static_cast<float>(x), static_cast<float>(y));
+    auto deltaPos = pos - m_prevMousePos;
+
+    const float cameraRotSpeed = 0.5f;
+
+    m_cameraYaw -= deltaPos.x * cameraRotSpeed;
+    m_cameraPitch -= deltaPos.y * cameraRotSpeed;
+
+    if (m_cameraYaw < 0.0f)
+        m_cameraYaw += 360.0f;
+    if (m_cameraYaw > 360.0f)
+        m_cameraYaw -= 360.0f;
+
+    if (m_cameraPitch > 89.0f)
+        m_cameraPitch = 89.0f;
+    if (m_cameraPitch < -89.0f)
+        m_cameraPitch = -89.0f;
+
+    m_prevMousePos = pos;
+}
+
+void Context::MouseButton(int button, int action, double x, double y)
+{
+    if (button == GLFW_MOUSE_BUTTON_RIGHT)
+    {
+        if (action == GLFW_PRESS)
+        {
+            m_cameraControl = true;
+            m_prevMousePos = glm::vec2(static_cast<float>(x), static_cast<float>(y));
+        }
+        else if (action == GLFW_RELEASE)
+        {
+            m_cameraControl = false;
+        }
+    }
+}
---
+++ object_example/src/context.h
@@ -0,0 +1,94 @@
+#ifndef __CONTEXT_H__
+#define __CONTEXT_H__
+
+#include "common.h"
+#include "shader.h"
+#include "program.h"
+#include "buffer.h"
+#include "vertex_layout.h"
+#include "texture.h"
+#include "mesh.h"
+#include "model.h"
+
+CLASS_PTR(Context)
+class Context
+{
+public:
+    static ContextUPtr Create();
+    void Render();
+    void ProcessInput(GLFWwindow* window);
+    void Reshape(int width, int height);
+    void MouseMove(double x, double y);
+    void MouseButton(int button, int action, double x, double y);
+    
+private:
+    Context() {}
+    bool Init();
+    
+    ProgramUPtr m_program;
+    ProgramUPtr m_simpleProgram;
+
+    MeshUPtr m_box;
+    ModelUPtr m_model;
+
+    TextureUPtr m_texture;
+    TextureUPtr m_texture2;
+
+    // camera parameter
+    glm::vec3 m_cameraPos { glm::vec3(0.0f, 0.0f, 3.0f) };
+    glm::vec3 m_cameraDir { glm::vec3(0.0f, 0.0f, -1.0f) };
+    glm::vec3 m_cameraUp { glm::vec3(0.0f, 1.0f, 0.0f) };
+    float m_cameraYaw { 0.0f };
+    float m_cameraPitch { 0.0f };
+
+    bool m_cameraControl { false };
+    glm::vec2 m_prevMousePos { glm::vec2(0.0f) };
+
+    // window size
+    int m_width { 1280 };
+    int m_height { 720 };
+
+    // animation
+    bool m_animation { true };
+
+    // ImGui Config Value
+    glm::vec4 m_clearColor { glm::vec4(0.1f, 0.2f, 0.3f, 1.0f) };
+
+    // light parameter
+    struct DirectionalLight {
+        glm::vec3 direction { glm::vec3(-2.0f, -1.0f, -3.0f) };
+        glm::vec3 ambient { glm::vec3(0.1f, 0.1f, 0.1f) };
+        glm::vec3 diffuse { glm::vec3(0.5f, 0.5f, 0.5f) };
+        glm::vec3 specular { glm::vec3(1.0f, 1.0f, 1.0f) };
+    };
+
+    struct PointLight {
+        glm::vec3 position { glm::vec3(3.0f, 3.0f, 3.0f) };
+        float distance { 32.0f };
+        glm::vec3 ambient { glm::vec3(0.1f, 0.1f, 0.1f) };
+        glm::vec3 diffuse { glm::vec3(0.5f, 0.5f, 0.5f) };
+        glm::vec3 specular { glm::vec3(1.0f, 1.0f, 1.0f) };
+    };
+
+    struct SpotLight {
+        glm::vec3 position { glm::vec3(3.0f, 3.0f, 3.0f) };
+        glm::vec3 direction { glm::vec3(-1.0f, -1.0f, -1.0f) };
+        glm::vec2 cutoff { glm::vec2(20.0f, 5.0f) };
+        float distance { 32.0f };
+        glm::vec3 ambient { glm::vec3(0.1f, 0.1f, 0.1f) };
+        glm::vec3 diffuse { glm::vec3(0.5f, 0.5f, 0.5f) };
+        glm::vec3 specular { glm::vec3(1.0f, 1.0f, 1.0f) };
+    };
+    SpotLight m_light;
+
+    // material parameter
+    struct Material {
+        TextureUPtr diffuse;
+        TextureUPtr specular;
+        float shininess { 32.0f };
+    };
+    Material m_material;
+    
+};
+
+#endif // __CONTEXT_H__
