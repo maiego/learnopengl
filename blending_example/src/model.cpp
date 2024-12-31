d9ef08223b4b20201195922cf8d483121ab10794
ryongdev <maiego@naver.com>
2025-01-01 00:07:25 +0900

    blending_example 추가
    glEnable(GL_BLEND) 등 블렌딩 관련 gl 함수
    반투명일 때 생기는 문제점 (뒤에서 봤을 때)
    fragment shader에서 alpha값 보고 discard 하는 기법
    glEnable(GL_CULL_FACE)

---
+++ blending_example/src/model.cpp
@@ -0,0 +1,114 @@
+#include "model.h"
+
+ModelUPtr Model::Load(const std::string& filename)
+{
+    auto model = ModelUPtr(new Model());
+    if (!model->LoadByAssimp(filename))
+        return nullptr;
+    return std::move(model);
+}
+
+bool Model::LoadByAssimp(const std::string& filename)
+{
+    std::string filepath = std::string(MODEL_PATH) + filename;
+
+    Assimp::Importer importer;
+    auto scene = importer.ReadFile(filepath,
+        aiProcess_Triangulate |
+        aiProcess_FlipUVs);
+
+    if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode)
+    {
+        SPDLOG_ERROR("failed to load model: {}", filename);
+        return false;
+    }
+
+    auto dirname = filepath.substr(0, filepath.find_last_of("/"));
+    auto LoadTexture = [&](aiMaterial* material, aiTextureType type) -> TexturePtr{
+        if (material->GetTextureCount(type) <= 0)
+            return nullptr;
+        
+        aiString filepath;
+        material->GetTexture(type, 0, &filepath);
+        auto image = Image::Load(fmt::format("{}/{}", dirname, filepath.C_Str()));
+        if (!image)
+            return nullptr;
+        return Texture::CreateFromImage(image.get());
+    };
+
+    for (uint32_t i = 0; i < scene->mNumMaterials; ++i)
+    {
+        auto material = scene->mMaterials[i];
+        
+        auto glMaterial = Material::Create();
+        glMaterial->diffuse = LoadTexture(material, aiTextureType_DIFFUSE);
+        glMaterial->specular = LoadTexture(material, aiTextureType_SPECULAR);
+
+        m_materials.push_back(std::move(glMaterial));
+    }
+    
+    ProcessNode(scene->mRootNode, scene);
+    return true;
+}
+
+void Model::ProcessNode(aiNode* node, const aiScene* scene)
+{
+    for (unsigned int i = 0; i < node->mNumMeshes; ++i)
+    {
+        auto meshIndex = node->mMeshes[i];
+        auto mesh = scene->mMeshes[meshIndex];
+        ProcessMesh(mesh, scene);
+    }
+
+    for (unsigned int i = 0; i < node->mNumChildren; ++i)
+    {
+        ProcessNode(node->mChildren[i], scene);
+    }
+}
+
+void Model::ProcessMesh(aiMesh* mesh, const aiScene* scene)
+{
+    std::vector<Vertex> vertices;
+    std::vector<unsigned int> indices;
+
+
+    vertices.resize(mesh->mNumVertices);
+    for (unsigned int i = 0; i < mesh->mNumVertices; ++i)
+    {
+        Vertex& vertex = vertices[i];
+        vertex.position = glm::vec3(mesh->mVertices[i].x, mesh->mVertices[i].y, mesh->mVertices[i].z);
+        vertex.normal = glm::vec3(mesh->mNormals[i].x, mesh->mNormals[i].y, mesh->mNormals[i].z);
+        if (mesh->mTextureCoords[0])
+        {
+            vertex.texCoord = glm::vec2(mesh->mTextureCoords[0][i].x, mesh->mTextureCoords[0][i].y);
+        }
+        else
+        {
+            vertex.texCoord = glm::vec2(0.0f, 0.0f);
+        }
+    }
+
+    indices.resize(mesh->mNumFaces * 3);
+    for (unsigned int i = 0; i < mesh->mNumFaces; ++i)
+    {
+        indices[3*i + 0] = mesh->mFaces[i].mIndices[0];
+        indices[3*i + 1] = mesh->mFaces[i].mIndices[1];
+        indices[3*i + 2] = mesh->mFaces[i].mIndices[2];
+    }
+    
+    auto glMesh = Mesh::Create(vertices, indices, GL_TRIANGLES);
+    if (mesh->mMaterialIndex >= 0 && mesh->mMaterialIndex < m_materials.size())
+    {
+        glMesh->SetMaterial(m_materials[mesh->mMaterialIndex]);
+    }
+
+    m_meshes.push_back(std::move(glMesh));
+}
+
+void Model::Draw(const Program* program) const
+{
+    for (const auto& mesh : m_meshes)
+    {
+        mesh->Draw(program);
+    }
+}
